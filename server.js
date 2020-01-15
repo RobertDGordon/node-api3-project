989c4ed1cbbd8f020e9d5d5b36fd3545138bdfbe
Robert Gordon <Komeuppance@gmail.com>
2020-01-15 14:26:26 -0800

    Added validateUser and validatePost

--- server.js
+++ server.js
@@ -27,4 +27,24 @@ function validateUserId(req, res, next) {
   }
 }
 
+function validateUser(res, req, next) {
+  if (req.body){
+    next()
+  }else if (req.body && !req.body.name) {
+    res.status(400).json({ message: 'Missing required name field'})
+  }else {
+    res.status(400).json({ message: 'Missing required user data'})
+  }
+}
+
+function validatePost(res, req, next) {
+  if (req.body){
+    next()
+  }else if (req.body && !req.body.text) {
+    res.status(400).json({ message: 'Missing required text field'})
+  }else {
+    res.status(400).json({ message: 'Missing required post data'})
+  }
+}
+
 module.exports = server;
